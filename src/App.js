3aa2a1a1d136991469fdf7aa2e8583697e3f2356
Nebojša Draganić <nebojsa311@gmail.com>
2020-05-05 23:37:08 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    //states
+    this.state = {
+      breakL: 300000,
+      sessionL: 1500000,
+      timerLabel: "Session",
+      timer: 1502000,
+    }
+    // Methods binding .this
+  }
+
+  render(){
+    return(
+      <div id="container">
+        <div id="main-title">Pomodoro clock</div>
+        <div class="length">
+          <div id="session-label">Session length</div>
+          <div id="session-length">{this.state.sessionL / 60000}</div>
+          <div id="session-increment"></div>
+          <div id="session-decrement"></div>
+        </div>
+        <div class="length">
+          <div id="break-label">Break length</div>
+          <div id="break-length">{this.state.breakL / 60000}</div>
+          <div id="break-increment"></div>
+          <div id="break-decrement"></div>
+        </div>
+        <div id="controls-div">
+          <div id="timer-label">{this.state.timerLabel}</div>
+          <div id="time-left">{(this.state.timer - this.state.timer%60000)/60000}:{this.state.timer/1000 - ((this.state.timer - this.state.timer%60000)/1000)}</div>
+          <div id="start_stop"></div>
+          <div id="reset"></div>
+        </div>
+      </div>
+
+    )
+  }
 }
 
 export default App;
